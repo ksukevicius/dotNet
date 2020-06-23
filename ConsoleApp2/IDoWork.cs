c10befb2846fb730e0075ea9fd15ed4503f46f0a
Kestutis <k.sukevicius@gmail.com>
2020-06-23 21:39:36 +0300

    Initial commit of my first interface

---
+++ ConsoleApp2/IDoWork.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDemoAppNSpace
+{
+    /// <summary>
+    /// just demo interface for learning purposes
+    /// </summary>
+
+    interface IDoWork
+    {
+        void printMsg();
+        int addNumbers(int a, int b);
+    }
+}
